477b3147a8b0072d8935c8e7a76a27ba789ac881
Manan Pal <devclannsit@gmail.com>
2016-10-16 21:29:30 +0530

    Made code PEP8 compilant

--- mysite/mapify/migrations/0001_initial.py
+++ mysite/mapify/migrations/0001_initial.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.utils.timezone
+import django.utils.timezone as dut
 
 
 class Migration(migrations.Migration):
@@ -17,12 +17,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TrafficModel',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
                 ('avg_speed', models.FloatField()),
                 ('latitude', models.FloatField()),
                 ('longitude', models.FloatField()),
                 ('place_id', models.CharField(max_length=100)),
-                ('record_time', models.DateField(default=django.utils.timezone.now)),
+                ('record_time', models.DateField(default=dut.now)),
             ],
         ),
     ]
--- mysite/mapify/urls.py
+++ mysite/mapify/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^request_static', views.request_static, name='req_st'),
-	url(r'^request_dynamic', views.request_dynamic, name='req_dy'),
-	url(r'^post_data', views.post_current_data, name='post_data')
+    url(r'^request_static', views.request_static, name='req_st'),
+    url(r'^request_dynamic', views.request_dynamic, name='req_dy'),
+    url(r'^post_data', views.post_current_data, name='post_data')
 ]
